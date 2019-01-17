31980225d987ea62d96bce5ad46e94198d7aa92e
Pavlo <pawel.wowniuk@gmail.com>
2019-01-17 13:19:32 +0100

    Zadanie domowe 3.3(Zrobiłem wyświetlanie w metodzie main)

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         Methods objekt1 = new Methods ();
-        objekt1.isEven (10);
-        objekt1.isOdd (10);
-        objekt1.circleField (4.2);
-        objekt1.power (3);
+        System.out.println (objekt1.isEven (10));
+        System.out.println (objekt1.isOdd (10));
+        System.out.println (objekt1.circleField (4.2));
+        System.out.println (objekt1.power (3));
     }
 }
